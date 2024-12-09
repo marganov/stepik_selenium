3a4d13c67f15a7c40e53b2c412d19346fdcebc2d
Alexei Marganov <marganovalexey@gmail.com>
2024-12-09 16:19:19 +0300

    test

--- Test_store_languages/test_items.py
+++ Test_store_languages/test_items.py
@@ -1,8 +1,23 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_button(self):
-
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+def test_add_to_cart_button(browser, link, user_language):
+    """
+    Тест проверяет наличие кнопки "Добавить в корзину" на странице товара.
+    """
+    # Устанавливаем язык интерфейса через параметр `user_language`
+    browser.get(f"{link}?lang={user_language}")
     
+    # Устанавливаем явное ожидание появления кнопки
+    try:
+        add_to_cart_button = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#add_to_basket_form > button"))
+        )
+        # Проверяем, что кнопка видима
+        assert add_to_cart_button.is_displayed(), "Кнопка 'Добавить в корзину' не отображается!"
+    except Exception:
+        pytest.fail("Кнопка 'Добавить в корзину' не найдена на странице.")
